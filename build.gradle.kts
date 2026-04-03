5bb4f8e34dc850e4a28f0544f88cd2f2294aeeea
yanlizca1 <emrahyenel@gmail.com>
2026-04-03 08:37:22 +0300

    Add top-level build configuration for Android project

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
