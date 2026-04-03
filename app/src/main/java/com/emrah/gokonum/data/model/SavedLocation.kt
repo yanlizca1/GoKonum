3bdac00ad9e5be92a652a602cf803dfa8b7a97c6
yanlizca1 <emrahyenel@gmail.com>
2026-04-03 08:35:59 +0300

    Update SavedLocation.kt

--- app/src/main/java/com/emrah/gokonum/data/model/SavedLocation.kt
+++ app/src/main/java/com/emrah/gokonum/data/model/SavedLocation.kt
@@ -1,1 +1,13 @@
 
+package com.emrah.gokonum.data.model
+
+import java.util.Date
+
+data class SavedLocation(
+    val id: Long = 0,
+    val name: String,
+    val latitude: Double,
+    val longitude: Double,
+    val note: String = "",
+    val date: Date = Date()
+)
