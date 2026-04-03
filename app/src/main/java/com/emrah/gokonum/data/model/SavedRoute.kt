a6dff042399e4dd822aaac7c4d3e246b61f1cbc7
yanlizca1 <emrahyenel@gmail.com>
2026-04-03 08:41:54 +0300

    Update SavedRoute.kt

--- app/src/main/java/com/emrah/gokonum/data/model/SavedRoute.kt
+++ app/src/main/java/com/emrah/gokonum/data/model/SavedRoute.kt
@@ -1,13 +1,16 @@
-
 package com.emrah.gokonum.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Date
 
+@Entity(tableName = "saved_route")
 data class SavedRoute(
+    @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     val name: String,
-    val totalDistance: Double,     // km
-    val duration: String,          // "45 dk" gibi
+    val totalDistance: Double,
+    val duration: String,
     val date: Date = Date(),
     val locations: List<SavedLocation> = emptyList()
 )
