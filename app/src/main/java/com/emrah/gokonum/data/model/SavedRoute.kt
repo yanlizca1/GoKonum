e367333d906c64097e0505d227e2d4787426ea8f
yanlizca1 <emrahyenel@gmail.com>
2026-04-03 08:36:18 +0300

    Add SavedRoute data class with properties

--- app/src/main/java/com/emrah/gokonum/data/model/SavedRoute.kt
+++ app/src/main/java/com/emrah/gokonum/data/model/SavedRoute.kt
@@ -1,1 +1,13 @@
 
+package com.emrah.gokonum.data.model
+
+import java.util.Date
+
+data class SavedRoute(
+    val id: Long = 0,
+    val name: String,
+    val totalDistance: Double,     // km
+    val duration: String,          // "45 dk" gibi
+    val date: Date = Date(),
+    val locations: List<SavedLocation> = emptyList()
+)
